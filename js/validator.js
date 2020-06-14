05cb33b411a989ac3eabdcf309482f7616991e6e
Sergey Chernik <34623368+owlbright@users.noreply.github.com>
2020-06-14 10:09:09 +0200

    Update validator.js

--- js/validator.js
+++ js/validator.js
@@ -24,9 +24,6 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  * ======================================================================== */
-const jQuery = require('jquery');
-const $ = jQuery;
-
 +function () {
   'use strict';
 
